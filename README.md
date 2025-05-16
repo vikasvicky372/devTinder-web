490abbd60316eb6036ec766b0cd436dc575ffb87
Vikas Thulasi <vikasvicky372@gmail.com>
2025-05-16 21:49:15 +0530

    login+axios+cors

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ create navbar component
 installed react router
 created components {Body,login, profile,footer, connections}
 created children routes inside App.jsx
+created Login component by daisyui
+installed axios library
+imported cors for the cors error
+added withcredentials:true for to store the cookies
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
-
+import React from "react";
+import axios from "axios";
 const Login = () => {
+  const [email, setEmail] = React.useState("vikas@gmail.com");
+  const [password, setPassword] = React.useState("Vikas@2000");
+  const handleLogin = async () => {
+    const data = await axios.post(
+      "http://localhost:1234/login",
+      {
+        emailId: email,
+        password: password,
+      },
+      { withCredentials: true }
+    );
+    console.log(data);
+  };
   return (
-    <div>Login</div>
-  )
-}
+    <div className="flex justify-center items-center my-20">
+      <div className="card card-dash bg-base-300 w-96">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+              </g>
+            </svg>
+            <input
+              type="email"
+              placeholder="mail@site.com"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.currentTarget.value)}
+            />
+          </label>
+          <div className="validator-hint hidden">Enter valid email address</div>
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+              </g>
+            </svg>
+            <input
+              type="password"
+              required
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.currentTarget.value)}
+            />
+          </label>
+          <div className="card-actions justify-center">
+            <button
+              className="btn btn-primary"
+              onClick={() => {
+                handleLogin();
+              }}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
