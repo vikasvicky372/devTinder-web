358272041613cdb24d40bdb8e33fe088f6186ce1
Vikas Thulasi <vikasvicky372@gmail.com>
2025-05-17 08:17:27 +0530

    profile/view added in body component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from './NavBar'
 import { Outlet } from 'react-router'
 import Footer from './Footer'
+import { BASE_URL } from '../utils/constants'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { addUser } from '../utils/userSlice'
 
 const Body = () => {
+
+  const userData = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const fetchUser = async () => {
+    try{
+      const response = await axios.get(BASE_URL+"/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(response.data));
+      console.log(response);
+    }catch(err){
+      if(err.status === 401){
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  }
+  useEffect(() => {
+    if(!userData){
+      fetchUser();
+    }
+  }, [])
   return (
     <div>
         <NavBar/>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
@@ -7,9 +8,9 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost hover:bg-info-content text-xl">
+        <Link to="/" className="btn btn-ghost hover:bg-info-content text-xl">
           👨‍💻 devTinder
-        </a>
+        </Link>
       </div>
       {user && (
       <div className="flex gap-2">
@@ -29,10 +30,10 @@ const NavBar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
